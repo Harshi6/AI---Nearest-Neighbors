4cc593ba24305e4cb41ae30c2ef5e980146e506e
Harshi6 <56491925+Harshi6@users.noreply.github.com>
2022-11-21 22:40:27 -0800

    Updating main

--- feature.py
+++ feature.py
@@ -1,12 +1,19 @@
+import csv
+import numpy as np
+import pandas as pd
+
+
+
 def main():
 	print("Welcome to Harshi Doddapaneni\'s Feature Selection Algorithm!")
 
 	IN_FILE = input("Type in the name of the file to test: ")
+	#reading the file
 	test = open(IN_FILE, 'r')
 	read = csv.reader(test,delimiter=' ',skipinitialspace=True)
 
 	features_count = len(next(read)) 
-
+	#find the number of instances
 	inst = pd.read_csv(IN_FILE)
 	instances_count= len(inst) + 1
 
@@ -20,10 +27,20 @@ def main():
 
 	print("This dataset has {} features (not including the class attribute), with {} instances\n".format(fc,instances_count))
 
+	fts = []
+	for i in range(1, features_count):
+		fts.append(i)
+	df = pd.read_fwf(IN_FILE, header=None)
+	data = df.copy(deep=True)[:-1]
+	acc = leave_one_out(features_count,data,fts)
+	print("Running nearest neighbor with all {} features, using \"leaving-one-out\" evalutation, I get an accuracy of {}%\n".format(fc, acc))
+
 
 	if alg == 1:
 
+		start = time.time()
 		forward_selection(features_count,IN_FILE,start)
 	if alg == 2:
-	
+
+		start = time.time()
 		backward_elimination(features_count,IN_FILE,start)
